33ab48c92a281a345f76c1e54ca1ce1114e948cd
Chebihi <chebihi.infinitysoft@google.com>
2022-02-28 11:12:21 +0100

    add centerPanel

--- src/Programme.java
+++ src/Programme.java
@@ -19,15 +19,27 @@ public class Programme {
         JPanel northPanel = new JPanel();
         FlowLayout flowLayout = new FlowLayout();
         flowLayout.setHgap(20);
+        flowLayout.setAlignment(FlowLayout.LEFT);
         northPanel.setLayout(flowLayout);
         northPanel.add(new JLabel("Pseudo : Killer"));
         northPanel.add(new Checkbox("enable"));
+
+        JPanel centerPanel = new JPanel();
+        GridLayout gridLayout = new GridLayout(2,2);
+        JLabel nom_lbl = new JLabel("Nom :");
+        nom_lbl.setFont(new Font("Arial",Font.BOLD,20));
+        centerPanel.add(nom_lbl);
+        centerPanel.add(new JTextField());
+        JLabel prenom_lbl = new JLabel("Prenom :");
+        prenom_lbl.setFont(new Font("Arial",Font.BOLD,20));
+        centerPanel.add(prenom_lbl);
+        centerPanel.add(new JTextField());
 
         mainPan.add(northPanel,BorderLayout.NORTH);
         mainPan.add(new JButton("South"),BorderLayout.SOUTH);
         mainPan.add(new JButton("East"),BorderLayout.EAST);
         mainPan.add(new JButton("West"),BorderLayout.WEST);
-        mainPan.add(new JButton("Center"),BorderLayout.CENTER);
+        mainPan.add(centerPanel,BorderLayout.CENTER);
 
 
         frame.setContentPane(mainPan);
