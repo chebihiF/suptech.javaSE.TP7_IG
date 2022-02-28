48d2b14ae49c036ab61c624319bc91d39fc446b3
Chebihi <chebihi.infinitysoft@google.com>
2022-02-28 10:20:19 +0100

    add northPanel

--- src/Programme.java
+++ src/Programme.java
@@ -13,10 +13,17 @@ public class Programme {
         JPanel mainPan = new JPanel();
         BorderLayout borderLayout = new BorderLayout();
         borderLayout.setHgap(10);
-        borderLayout.setVgap(30);
+        borderLayout.setVgap(10);
         mainPan.setLayout(borderLayout);
 
-        mainPan.add(new JButton("North"),BorderLayout.NORTH);
+        JPanel northPanel = new JPanel();
+        FlowLayout flowLayout = new FlowLayout();
+        flowLayout.setHgap(20);
+        northPanel.setLayout(flowLayout);
+        northPanel.add(new JLabel("Pseudo : Killer"));
+        northPanel.add(new Checkbox("enable"));
+
+        mainPan.add(northPanel,BorderLayout.NORTH);
         mainPan.add(new JButton("South"),BorderLayout.SOUTH);
         mainPan.add(new JButton("East"),BorderLayout.EAST);
         mainPan.add(new JButton("West"),BorderLayout.WEST);
